2a1db2facf1a28626718a0660339ea5d6f1016fd
Bruno <bruno.conrad12@gmail.com>
2022-09-11 11:46:09 -0300

    Adicionado componentes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import Header from "./componentes/Header";
+import SalesCard from "./componentes/SalesCard";
+
 function App() {
-  return(
-    <h1>Olá mundo</h1>
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+        </section>
+      </main>
+    </>
   )
 }
 
-export default App
+export default App;
